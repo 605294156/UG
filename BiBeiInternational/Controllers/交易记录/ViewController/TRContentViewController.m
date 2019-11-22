d1d87e922fcf4fdfc9a0a53b30c6f3f23f7342ac
cheng <888888@SB.com>
2019-11-22 15:55:31 +0800

    no message

--- BiBeiInternational/Controllers/交易记录/ViewController/TRContentViewController.m
+++ BiBeiInternational/Controllers/交易记录/ViewController/TRContentViewController.m
@@ -246,9 +246,9 @@
     UGOredeModel *orderModel = self.dataSource[indexPath.section];
     //新修改 查看资产
     if ([orderModel.status isEqualToString:@"3"]) {
-        return 150.0f;
+        return 132.0f;
     }
-    return self.isOTC ? 150.0f: 145.0f;
+    return self.isOTC ? 132.0f: 145.0f;
 }
 
 #pragma mark-撤销
