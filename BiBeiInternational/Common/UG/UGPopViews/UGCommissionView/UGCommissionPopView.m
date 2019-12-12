38eaad2145b8f812818009d32867d8397700f52f
Leven <>
2019-12-12 14:24:08 +0800

    币币交换页面弹框修改

--- BiBeiInternational/Common/UG/UGPopViews/UGCommissionView/UGCommissionPopView.m
+++ BiBeiInternational/Common/UG/UGPopViews/UGCommissionView/UGCommissionPopView.m
@@ -17,15 +17,15 @@
 @property (nonatomic,copy)NSString *title;
 @property (nonatomic,copy)NSString *tureStr;
 @property (nonatomic ,copy) NSArray *titles;
-@property (nonatomic,strong)UGButton *cancelBtn;
-@property (nonatomic,strong)UGButton *tureBtn;
+@property (nonatomic,strong)UIButton *cancelBtn;
+@property (nonatomic,strong)UIButton *tureBtn;
 @property (copy, nonatomic) void(^clickHandle)(void);
 @property(nonatomic, strong)PopView *popView;
 - (instancetype)initWithTitle:(NSString *)title Titles:(NSArray <NSString *>*)titles WithTureBtn:(NSString *)tureStr clickItemHandle:(void(^)(void))clickHandle;
 @end
 
-#define titleHeight UG_AutoSize(50)
-#define btnHeight UG_AutoSize(40)
+#define titleHeight UG_AutoSize(58)
+#define btnHeight UG_AutoSize(46)
 #define space  UG_AutoSize(15)
 #define CancelTag 6666
 #define TureTag 8888
@@ -39,7 +39,7 @@
         }
     }];
     PopView *popView =[PopView popSideContentView:centerView direct:PopViewDirection_SlideInCenter];
-    popView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.3];
+    popView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.6];
     popView.didRemovedFromeSuperView = ^{
         [centerView removeFromSuperview];
     };
@@ -53,13 +53,13 @@
 {
     self = [super init];
     if (self) {
-        CGRect frame = CGRectMake(UG_AutoSize(32), (kWindowH-UG_AutoSize(336))/2, kWindowW-2*UG_AutoSize(32), UG_AutoSize(336));
+        CGRect frame = CGRectMake(UG_AutoSize(52), (kWindowH-UG_AutoSize(310))/2, kWindowW-2*UG_AutoSize(52), UG_AutoSize(310));
         self = [super initWithFrame:frame];
         if (self) {
             self.frame = frame;
             self.clickHandle = clickHandle;
-            self.backgroundColor = [UIColor colorWithHexString:@"F8F9FC"];
-            self.layer.cornerRadius = 4;
+            self.backgroundColor = [UIColor colorWithHexString:@"f7f7f7"];
+            self.layer.cornerRadius = 2;
             self.title = title;
             self.tureStr = tureStr;
             self.titles = titles;
@@ -77,11 +77,15 @@
     [self addSubview:self.tableView];
     [self.tableView reloadData];
     
-    self.cancelBtn.frame = CGRectMake(UG_AutoSize(20), self.bounds.size.height-space-btnHeight, UG_AutoSize(90), btnHeight);
+    self.cancelBtn.frame = CGRectMake(0, self.bounds.size.height-btnHeight, self.bounds.size.width*0.5, btnHeight);
     [self addSubview:self.cancelBtn];
     
-    self.tureBtn.frame = CGRectMake(self.bounds.size.width-UG_AutoSize(20)-UG_AutoSize(90), self.bounds.size.height-space-btnHeight, UG_AutoSize(90), btnHeight);
+    self.tureBtn.frame = CGRectMake(self.bounds.size.width*0.5, self.bounds.size.height-btnHeight, self.bounds.size.width*0.5, btnHeight);
     [self addSubview:self.tureBtn];
+    
+    UILabel *lineLab = [[UILabel alloc] initWithFrame:CGRectMake(0, self.bounds.size.height-btnHeight-0.5, self.bounds.size.width, 0.5)];
+    lineLab.backgroundColor = [UIColor colorWithHexString:@"dddddd"];
+    [self addSubview:lineLab];
 }
 
 #pragma mark-取消、确定
@@ -121,7 +125,7 @@
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return UG_AutoSize(44);
+    return UG_AutoSize(40);
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
@@ -134,7 +138,7 @@
 #pragma mark-UITableView
 - (UITableView *)tableView{
     if (!_tableView) {
-        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, titleHeight, self.bounds.size.width,self.bounds.size.height-titleHeight-2*space-btnHeight) style:UITableViewStylePlain];
+        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, titleHeight-10, self.bounds.size.width,self.bounds.size.height-titleHeight-btnHeight) style:UITableViewStylePlain];
         _tableView.delegate = self;
         _tableView.dataSource = self;
         _tableView.backgroundColor = [UIColor clearColor];
@@ -151,32 +155,35 @@
         _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.bounds.size.width, UG_AutoSize(54))];
         _titleLabel.text =self.title;
         _titleLabel.font = UG_AutoFont(18);
+        _titleLabel.textColor = [UIColor colorWithHexString:@"333333"];
         _titleLabel.textAlignment = NSTextAlignmentCenter;
     }
     return _titleLabel;
 }
 
 #pragma mark -取消按钮
--(UGButton *)cancelBtn{
+-(UIButton *)cancelBtn{
     if (!_cancelBtn) {
-        _cancelBtn = [[UGButton alloc]initWithUGStyle:UGButtonStyleWhite];
-        _cancelBtn.layer.cornerRadius = 4;
+        _cancelBtn = [[UIButton alloc] init];
+        [_cancelBtn setBackgroundColor:[UIColor colorWithHexString:@"f7f7f7"]];
         [_cancelBtn setTitle:@"取消" forState:UIControlStateNormal];
         _cancelBtn.tag = CancelTag;
         _cancelBtn.titleLabel.font = UG_AutoFont(16);
+        [_cancelBtn setTitleColor:[UIColor colorWithHexString:@"6784c7"] forState:UIControlStateNormal];
         [_cancelBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _cancelBtn;
 }
 
 #pragma mark- 确认按钮
--(UGButton *)tureBtn{
+-(UIButton *)tureBtn{
     if (!_tureBtn) {
-        _tureBtn = [[UGButton alloc]initWithUGStyle:UGButtonStyleBlue];
+        _tureBtn = [[UIButton alloc] init];
+        [_tureBtn setBackgroundColor:[UIColor colorWithHexString:@"6784c7"]];
         [_tureBtn setTitle:self.tureStr forState:UIControlStateNormal];
         _tureBtn.tag = TureTag;
-        _tureBtn.layer.cornerRadius = 4;
         _tureBtn.titleLabel.font = UG_AutoFont(16);
+        [_tureBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_tureBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _tureBtn;
